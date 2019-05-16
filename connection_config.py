f061b3c781e6ddf31dfbd6a2e68c641658edecb9
Luca Mannella <luca.mannella@linksfoundation.com>
2019-05-16 11:50:20 +0200

    Ping logic changed.
    Now the ip address in GOST_address file is specified with "http" prefix and with the relative port.

--- connection_config.py
+++ connection_config.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
+import requests
 
 address_file_path = './config_files/GOST_address.txt'
 
 
 def get_address_from_file():
-    """reads and returns the address saved in address_file_path"""
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    """ Reads and returns the address saved in address_file_path. """
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(address_file_path) as fp:
         new_address = fp.readline()
         return new_address
@@ -23,13 +24,39 @@ def set_GOST_address(address=None):
         os.write(f, bytes(address, 'utf-8'))
         os.fsync(f)
         os.close(f)
-    if test_connection(address[:-5]):
+    if test_connection(address):  # [:-5]):
         return address
     else:
         return False
 
 
-def test_connection(address, verbose=False):
+def test_connection(server_address, server_username=None, server_password=None):
+    """ This function checks if a REST connection is correctly configured.
+
+    :param server_address: The address of the OGC server.
+    :param server_username: The username necessary to be authenticated on the server.
+    :param server_password: The password related to the given username.
+    :return: True if it is possible to establish a connection, False otherwise.
+    """
+
+    try:
+        if server_username is None and server_password is None:
+            r = requests.get(url=server_address)
+        else:
+            r = requests.get(url=server_address, auth=(server_username, server_password))
+        if r.ok:
+            print("Network connectivity: VERIFIED. Server "+server_address+" is reachable!")
+            return True
+        else:
+            print("Something wrong during connection!")
+            return False
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+def old_test_connectivity(address, verbose=False):
     if verbose:
         response = os.system("ping " + address)
     else:
