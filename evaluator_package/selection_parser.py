703e04a80bcea94b49b51c65ad3f811c6bc0e70d
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2019-05-20 22:15:43 +0200

    Fixed "in"  evaluation rule

--- evaluator_package/selection_parser.py
+++ evaluator_package/selection_parser.py
@@ -53,16 +53,19 @@ def a(tokens, record):
         tokens.pop(0)
         if tokens[0] == "not":
             tokens.pop(0)
+            if tokens[0] == "in":
+                tokens.pop(0)
+                temp_field = record[tokens[0]]
+                tokens.pop(0)
+                return not (temp_val in temp_field)
+            else:
+                return parse_error()
+        elif tokens[0] == "in":
             tokens.pop(0)
-            temp_field = record[tokens[0]]
-            tokens.pop(0)
-            return not (temp_val in temp_field)
-        elif is_field(tokens[0]):
             temp_field = record[tokens[0]]
             tokens.pop(0)
             return temp_val in temp_field
-        else:
-            tokens.pop(0)
+
     elif is_field(tokens[0]):
         temp_field = record[tokens[0]]
         tokens.pop(0)
@@ -87,8 +90,8 @@ def parse_error():
 
 def is_field(token):
     return token in ["name", "description", "encodingType", "location", "properties", "metadata",
-                        "definition", "phenomenonTime", "resultTime", "observedArea", "result", "resultQuality",
-                        "validTime", "time", "parameters", "feature"]
+                     "definition", "phenomenonTime", "resultTime", "observedArea", "result", "@iot.id",
+                     "resultQuality","validTime", "time", "parameters", "feature"]
 def is_value(token):
     return not (is_field(token) or token in ["(", ")", "and", "or", "in", "not"])
 
@@ -119,12 +122,19 @@ def test(record):
                 "tokens": ["name", "==", "mario","or", "name", "==", "gianni",
                            "and", "(", "1", "in", "metadata", "and", "2", "not", "in", "description", ")"],
                 "expected_result": True}
-    tokens_9 = {"name": "8",
+    tokens_9 = {"name": "9",
                 "tokens": ["name", "!=", "mario", "or", "gianni", "not", "in", "name",
                            "and", "(", "1", "in", "metadata", "and", "2", "not", "in", "description", ")"],
                 "expected_result": True}
 
+    t_10 = tokens_9["tokens"] + ["and"] + tokens_2["tokens"]
+
+    tokens_10 = {"name": "10",
+                "tokens": t_10,
+                "expected_result": False}
+
     test_list = []
+
     test_list.append(tokens_1)
     test_list.append(tokens_2)
     test_list.append(tokens_3)
@@ -134,6 +144,7 @@ def test(record):
     test_list.append(tokens_7)
     test_list.append(tokens_8)
     test_list.append(tokens_9)
+    test_list.append(tokens_10)
 
     for i in test_list:
         result = parser(i["tokens"], record)
@@ -143,5 +154,5 @@ def test(record):
             print(f"test {i['name']} not passed")
 
 
-args = {"name" : "mario", "description" : "default description", "metadata" : "meta 1"}
+args = {"name" : "mario", "description" : "default description", "metadata" : "meta 1", "@iot.id" : "1"}
 test(args)
